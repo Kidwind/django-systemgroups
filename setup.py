87ee1e3be19494c00a678c8067504f416c70653e
Kidwind <Kidwind@gmail.com>
2017-01-11 14:56:57 +0800

    pipy package.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+VERSION = '1.0.0.dev1'
+
+setup(
+    name='django-systemgroups',
+    version=VERSION,
+    url='https://github.com/Kidwind/django-systemgroups',
+    author='Kidwind',
+    author_email='Kidwind@gmail.com',
+    description="基于Django实现的系统组。",
+    zip_safe=False,
+    packages=[
+        'systemgroups',
+    ],
+    keywords='django system groups',
+    license='BSD',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Security',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 ],
+)
